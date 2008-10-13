df43ff724219a63751cb40dd891ff7e0fa263dce
Jaykul <none@none>
2008-10-13 04:59:27 +0000

    Add demo of the "SnappingWindowsBehavior" to the GlobalHotkeys demo app.

--- GlobalHotkeys/DemoWindow.xaml.cs
+++ GlobalHotkeys/DemoWindow.xaml.cs
@@ -1,15 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Huddled.Interop.Hotkeys;
+using Huddled.Interop;
+using Huddled.Wpf;
 
 
 namespace GlobalHotkeys
