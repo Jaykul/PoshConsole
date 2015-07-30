7cbd492db909ed78e2d88c72cda7d30e1a6d1938
Joel Bennett <Jaykul@HuddledMasses.org>
2015-07-30 12:14:42 -0400

    Add .nuspec
    Switch to building Release by default
    Populate AssemblyInfo

--- PoshCode.PoshConsole/Properties/AssemblyInfo.cs
+++ PoshCode.PoshConsole/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
@@ -6,12 +7,12 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfControlLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PoshConsole")]
+[assembly: AssemblyDescription("The PoshConsole Control for hosting PowerShell in WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfControlLibrary1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("PoshCode.com")]
+[assembly: AssemblyProduct("PoshCode.PoshConsole")]
+[assembly: AssemblyCopyright("Copyright © Joel Bennett 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,5 +54,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
