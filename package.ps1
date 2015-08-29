6f1e673c479e07d7b4697f53e8b42735458ed43c
Joel Bennett <Jaykul@HuddledMasses.org>
2015-08-28 20:31:52 -0400

    Fix package script so I dare to use it

--- package.ps1
+++ package.ps1
@@ -1,8 +1,14 @@
+[CmdletBinding()]param()
+
+
 pushd $PSScriptRoot
-# msbuild /p:Configuration=Release
+msbuild /p:Configuration=Release
 nuget pack PoshCode.PoshConsole\PoshCode.PoshConsole.csproj -Symbols -IncludeReferencedProjects
+$package = ls .\PoshCode.PoshConsole.*[0-9].nupkg | Sort LastWriteTime -Desc | Select -First 1
 
-ls .\PoshCode.PoshConsole.*[0-9].nupkg | Sort LastWriteTime -Desc | Select -First 1
-
-# nuget push .\PoshCode.PoshConsole.0.6.2.0.nupkg
-# popd
+$RelativePath = (Resolve-Path $package -Relative)
+if($PSCmdlet.ShouldContinue("Are you sure you want to publish $RelativePath", "Publish $($package.Name)")) {
+    $package = (Resolve-Path $package)
+    nuget push $package
+}
+popd
