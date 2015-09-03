f3bbdf7b0ce6a2264d200af6eb4ca3e48543da4c
Joel Bennett <Jaykul@HuddledMasses.org>
2015-09-03 13:42:07 -0400

    Fix packaging script (and nuspec) so pushing symbols to SymbolSource.org works properly

--- package.ps1
+++ package.ps1
@@ -1,14 +1,21 @@
-[CmdletBinding()]param()
+[CmdletBinding()]param(
+    $ProjectName = "PoshCode.PoshConsole"
+)
 
 
 pushd $PSScriptRoot
 msbuild /p:Configuration=Release
-nuget pack PoshCode.PoshConsole\PoshCode.PoshConsole.csproj -Symbols -IncludeReferencedProjects
-$package = ls .\PoshCode.PoshConsole.*[0-9].nupkg | Sort LastWriteTime -Desc | Select -First 1
+nuget pack "${ProjectName}\${ProjectName}.csproj" -Symbols -IncludeReferencedProjects 
+nuget pack "${ProjectName}\${ProjectName}.csproj" -IncludeReferencedProjects -Exclude "**\*.pdb;**\*.cs"
+$package = ls "$PSScriptRoot\${ProjectName}.*[0-9].nupkg" | Sort LastWriteTime -Desc | Select -First 1
 
 $RelativePath = (Resolve-Path $package -Relative)
-if($PSCmdlet.ShouldContinue("Are you sure you want to publish $RelativePath", "Publish $($package.Name)")) {
-    $package = (Resolve-Path $package)
-    nuget push $package
+if(Test-Path $RelativePath) {
+    if($PSCmdlet.ShouldContinue("Are you sure you want to publish $RelativePath", "Publish $($package.Name)")) {
+        $package = (Resolve-Path $package)
+        nuget push $package
+        mkdir "${PSScriptRoot}\Releases" -Force
+        mv "$PSScriptRoot\${ProjectName}*.nupkg" "${PSScriptRoot}\Releases"
+    }
 }
 popd
