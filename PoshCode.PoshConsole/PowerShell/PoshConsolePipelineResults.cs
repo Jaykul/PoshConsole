4c9656579dbbe8ce6fe77a2691bb13ff6826710b
Joel Bennett <Jaykul@HuddledMasses.org>
2015-10-01 16:03:35 -0400

    Add helper methods for working with results and errors

--- PoshCode.PoshConsole/PowerShell/PoshConsolePipelineResults.cs
+++ PoshCode.PoshConsole/PowerShell/PoshConsolePipelineResults.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 
@@ -21,11 +22,57 @@ namespace PoshCode.PowerShell
         }
 
         public Collection<PSObject> Output { get; }
+
+        public IEnumerable<TResult> GetOutputObjects<TResult>()
+        {
+            return  Output.Select(pso => pso.BaseObject).Cast<TResult>();
+        }
+
         public Collection<object> Errors { get; }
+
+        public IEnumerable<KeyValuePair<string, Exception>> GetExceptions()
+        {
+            if (!HadErrors) return new KeyValuePair<string, Exception>[0];
+
+            return Errors.Select(AsException);
+        }
+
         public Collection<Command> Commands { get; }
 
         public bool HadErrors { get; }
         public long InstanceId { get; }
         public PipelineState State { get; }
+
+
+        private static KeyValuePair<string,Exception> AsException(object error)
+        {
+            var exception = error as Exception;
+            string message;
+            if (exception == null)
+            {
+                var er = error as ErrorRecord;
+                if (er != null)
+                {
+                    message = er.ErrorDetails.Message;
+                    exception = er.Exception;
+                }
+                else
+                {
+                    message = error.ToString();
+                }
+            }
+            else
+            {
+                message = exception.Message;
+            }
+
+
+            var cme = exception as CmdletInvocationException;
+            if (cme != null)
+            {
+                exception = cme.ErrorRecord.Exception;
+            }
+            return new KeyValuePair<string, Exception>(message, exception);
+        }
     }
 }
